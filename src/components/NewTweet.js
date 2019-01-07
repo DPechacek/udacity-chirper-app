f3519456d503f161072bae77bc8ee75eac3cb0b5
David Pechacek <Pechacek_David@bah.com>
2019-01-07 15:12:32 -0500

    React router implemented

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleAddTweet } from "../actions/tweets";
+import { Redirect } from 'react-router-dom';
 
 class NewTweet extends Component {
     state = {
-        text: ''
+        text: '',
+        toHome: false
     };
 
     handleChange = (event) => {
@@ -24,12 +26,17 @@ class NewTweet extends Component {
         dispatch(handleAddTweet(text, id));
 
         this.setState(() => ({
-            text: ''
+            text: '',
+            toHome: id ? false : true
         }))
     }
 
     render() {
-        const { text } = this.state;
+        const { text, toHome } = this.state;
+
+        if(toHome === true) {
+            return <Redirect to='/' />
+        }
 
         /* Redirect to the home view if submitted */
 
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline';
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline';
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline';
 import { handleToggleTweet } from "../actions/tweets";
+import { Link, withRouter } from 'react-router-dom';
 
 class Tweet extends Component {
 
@@ -22,6 +23,7 @@ class Tweet extends Component {
 
     toParent = (e, id) => {
         e.preventDefault();
+        this.props.history.push(`/tweet/${id}`);
     }
     
     render() {
@@ -31,12 +33,10 @@ class Tweet extends Component {
             return <p>This tweet does not exist.</p>
         }
 
-        const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet;
-
-
-
+        const { id, name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet;
+        
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img src={avatar} alt={`Avatar of ${name}`} className='avatar'/>
                 <div className='tweet-info'>
                     <div>
@@ -62,7 +62,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -77,4 +77,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
